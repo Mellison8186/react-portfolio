7a094584fbd1f08f11c358c12278fb56417bc854
maribels_are_ringing <maribel.duron86@gmail.com>
2021-06-17 14:26:35 -0700

    fixed LI path in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer() {
     return (
     <footer className="flex-row justify-content-center px-1 py-2">
         <a href="https://github.com/Mellison8186" target="blank"><img src={gitHub} alt="gitHub link" /></a>
-        <a href="www.linkedin.com/in/maribel-ellison" target="blank"><img src={linkedIn} alt="linkedIn link" /></a>
+        <a href="https://www.linkedin.com/in/maribel-ellison" target="blank"><img src={linkedIn} alt="linkedIn link" /></a>
         <a href="https://stackexchange.com/users/20478567/maribel-ellison" target="blank"><img src={stack} alt="stackoverflow link" /></a>
     </footer>
     )
