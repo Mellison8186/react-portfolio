5a0cf1e61e019f98e4db03d275b6ed3913dc963a
maribels_are_ringing <maribel.duron86@gmail.com>
2021-06-16 21:22:42 -0700

    updated app.js and package.json

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
     <App />
-    </Route>
-      </Switch>
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
